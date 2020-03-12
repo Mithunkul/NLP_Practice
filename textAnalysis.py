3eda67d0aad2b8998adf921a47a100660656686e
Mithunkul <61451018+Mithunkul@users.noreply.github.com>
2020-03-12 11:04:19 +0530

    Add files via upload

--- textAnalysis.py
+++ textAnalysis.py
@@ -2,18 +2,23 @@ from bs4 import BeautifulSoup
 from urllib import request
 import nltk, re, pprint
 from nltk import word_tokenize
+from nltk.corpus import stopwords
 
-class textAnalysis:
+class textAnalysis():
     
    
     #---------------------------------------------------------------------------------------------#
     
     def parseHTML(self, url):
         html = request.urlopen(url).read().decode('utf8')
-        raw = BeautifulSoup(html, 'html.parser').get_text()
+        soup = BeautifulSoup(html, "html.parser")
+        for script in soup(["script", "style"]):
+            script.decompose()
+        raw = soup.get_text()
         with open ("rawText.txt", "w", encoding = "utf-8") as f:
-            f.write(raw)
+            f.write(text)
         self.processRaw(raw)
+        
     
     
     #---------------------------------------------------------------------------------------------#
@@ -29,15 +34,15 @@ class textAnalysis:
     
     def processRaw(self, raw):
         tokens = word_tokenize(raw)
-        print(len(tokens))
+        stop_words = list(set(stopwords.words('english')))
+        filtered_tokens = [w for w in tokens if w not in stop_words] 
+        print(filtered_tokens)
     
     #---------------------------------------------------------------------------------------------#
     
-    def __init__(self, url = None, text = None, website_with_html = None):
-        if website_with_html == 1:
+    def __init__(self, url = None, text = None):
+        if url != None:
             self.parseHTML(url)
-        elif website_with_html ==0:
-            self.parseText(url)
         if text != None:
             with open ("rawText.txt", "w",  encoding = "utf-8") as f:
                 f.write(text)
@@ -46,4 +51,4 @@ class textAnalysis:
     #---------------------------------------------------------------------------------------------#
             
     
-textAnalysis(url = "https://www.goodreads.com/review/list/48483884-michael-finocchiaro?shelf=post-modern", website_with_html = 1)
+textAnalysis(text = "Whatever man, I don't care for your faggoted relationships.")
